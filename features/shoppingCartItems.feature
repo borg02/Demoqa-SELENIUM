64bc1594667592f0dc84dd933b320bb1c7b6de36
Frivicktz <fritz_molin@live.se>
2022-01-05 17:38:15 +0100

    Added common-backgrounds and fixed some small issues
    Common backgrounds are now added, so all of the popups should be dealt with. I also realized i had forgot to change When to Then on two occasions. Which i fixed.

--- features/shoppingCartItems.feature
+++ features/shoppingCartItems.feature
@@ -4,15 +4,19 @@ Feature: The shopping cart and the items in it should show up
   that i am able to buy them
 
 
+Background:
+  Given that we are on Willy's website
+  And that we accepted the standard cookie policy
+  And that we have have been through the initial where to deliver popup
+
 
 
 Scenario: Looking at my added items in the shopping cart
-  Given that i am on the shopping site
-  And have added items to my shopping cart
+  Given that have added items to my shopping cart
   When I Enter the shopping cart
   Then I should be able to see the items i have added
 
 Scenario: The shopping cart should show up when i click on the button for it
-  Given that i am on the shopping site
+  Given that it is the first time I am visiting the site
   When I Enter the shopping cart
   Then an empty shopping cart should show up
--- step-definitions/shoppingCartItems.js
+++ step-definitions/shoppingCartItems.js
@@ -1,19 +1,13 @@
 module.exports = function ()
 {
-  this.Given(/^that i am on the shopping site$/, async function ()
-  {
-    await helpers.loadPage('https://www.willys.se/');
-
-    await driver.manage().setTimeouts({ implicit: 10000 });
-    let cookieAcceptor = await driver.findElement(by.id('onetrust-accept-btn-handler'));
-    await cookieAcceptor.click();
-    await driver.sleep(1000);
-  });
-
-  this.Given(/^have added items to my shopping cart$/, async function () {
+  this.Given(/^that have added items to my shopping cart$/, async function () {
     let allButtons = await driver.findElements(By.css('button'));
     let acceptButton = allButtons[9];
     await acceptButton.click();
+    await driver.sleep(1000);
+  });
+
+  this.Given(/^that it is the first time I am visiting the site$/, async function () {
     await driver.sleep(1000);
   });
 
@@ -24,13 +18,14 @@ module.exports = function ()
     await driver.sleep(1000);
   });
   
-  this.When(/^I should be able to see the items i have added$/, async function () {
+  this.Then(/^I should be able to see the items i have added$/, async function () {
     let shoppingCartText = await driver.findElements(By.css('[class^="md-list-item-text col-summary"]'));
     let hasShoppingCartText = shoppingCartText.includes('Majskyckling Hel Sverige'); 
   });
 
 
-  this.When(/^an empty shopping cart should show up$/, async function () {
+
+  this.Then(/^an empty shopping cart should show up$/, async function () {
     let emptyShoppingCartText = await driver.findElements(By.id('selenium--miniCart-empty-info'));
     let hasEmptyShoppingCartText = emptyShoppingCartText.includes('Din varukorg är tom!');
   });
